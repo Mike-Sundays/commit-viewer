39839982a6e447dcf936be38d51311cb54372f49
Miguel Domingos <migueldomingos94@hotmail.com>
2019-11-22 12:24:26 +0000

    Add test to see if result is independent from source

---
+++ spec/lib/commit_lister/result_is_independend_of_source_spec.rb
@@ -0,0 +1,48 @@
+# these are tests that communicate directly with github
+require './lib/commit_lister/lister'
+require './lib/commit_lister/git_api/api_commit_parser'
+require './lib/commit_lister/git_api/api_commit_getter'
+require './lib/commit_lister/git_cli/cli_commit_parser'
+require './lib/commit_lister/git_cli/cli_commit_getter'
+
+RSpec.describe CommitLister::Lister do
+  before :all do
+    @url = "https://github.com/Mike-Sundays/simple-notes-react.git"
+    @page = 1
+    @per_page = 10
+    @api_parser = CommitLister::ApiCommitParser.new
+    @api_getter = CommitLister::ApiCommitGetter.new(@page, @per_page)
+    @cli_parser = CommitLister::CliCommitParser.new
+    @cli_getter = CommitLister::CliCommitGetter.new(@page, @per_page)
+  end
+
+  it "should return a default paginated list of commits" do
+    # this test will fail when I do to many requests due to the github throtler
+    api_result = CommitLister::Lister.new(@url, @api_parser, @api_getter).run
+    cli_result = CommitLister::Lister.new(@url, @cli_parser, @cli_getter).run
+
+    compare_results(api_result, cli_result)
+  end
+
+  it "should return a failure if url is invalid" do
+    api_result = CommitLister::Lister.new("", @api_parser, @api_getter).run
+    cli_result = CommitLister::Lister.new("", @cli_parser, @cli_getter).run
+
+    compare_results(api_result, cli_result)
+  end
+
+  it "should return a failure if repo does not exist" do
+    url = "https://github.com/Mike-Sundays/iehgsie.git"
+    api_result = CommitLister::Lister.new(url, @api_parser, @api_getter).run
+    cli_result = CommitLister::Lister.new(url, @cli_parser, @cli_getter).run
+
+    compare_results(api_result, cli_result)
+  end
+
+  def compare_results(api_result, cli_result)
+    expect(api_result.successful).to eql(cli_result.successful)
+    expect(api_result.data).to eql(cli_result.data)
+    expect(api_result.valid_input).to eql(cli_result.valid_input)
+    expect(api_result.error).to eql(cli_result.error)
+  end
+end
